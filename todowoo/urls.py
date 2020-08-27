7dfa4d572de5c5963e023c93d1ef9662143bf5dc
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-08-27 16:57:55 -0700

    Adding viewtodo, deletetodo

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 from todo import views
 
 urlpatterns = [
+    # Home page
     path('', views.home, name='home'),
 
+    # Admin page
     path('admin/', admin.site.urls),
 
     # Authorization
@@ -28,6 +30,9 @@ urlpatterns = [
     path('logout/', views.logoutuser, name='logoutuser'),
 
     # Todos
+    path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
-
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
